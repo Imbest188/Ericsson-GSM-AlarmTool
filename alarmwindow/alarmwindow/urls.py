e1c140558c97f5e4799545b7c0a3082117526a83
Imbest188 <61537414+Imbest188@users.noreply.github.com>
2022-01-31 00:38:02 +0300

    Update urls.py

--- alarmwindow/alarmwindow/urls.py
+++ alarmwindow/alarmwindow/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.AlarmView.as_view(), name='alarms'),
     path('alarms', views.AlarmView.as_view(), name='alarms'),
     path('update_alarms', views.AlarmView.as_view(), name='update_alarms'),
 ]
