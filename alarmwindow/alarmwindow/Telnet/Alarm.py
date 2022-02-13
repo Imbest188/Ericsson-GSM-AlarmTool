08f1188b62461201385f1fa16c63a9cbe565c154
Imbest188 <61537414+Imbest188@users.noreply.github.com>
2022-02-13 15:13:27 +0300

    Update Alarm.py

--- alarmwindow/alarmwindow/Telnet/Alarm.py
+++ alarmwindow/alarmwindow/Telnet/Alarm.py
@@ -1,6 +1,12 @@
 import re
 from datetime import datetime as dt
 
+object_name_keys = ['RSITE', 'TG', 'SAID', 'SPID']
+managed_object_keys = ['MO', 'CELL', 'DIP', 'SDIP', 'LAYER', 'DEST', 'UNIT', 'FILENAME', 'SNT']
+slogan_keys = ['ALARM_SLOGAN', 'FAULT', 'FAULT_TYPE', 'REASON', 'STATE']
+
+ALARM_TYPE_REGEXP = r"[A|O][1-3]\/?\w{3}"
+
 
 class Alarm:
     def mark_as_ceased(self):
@@ -16,21 +22,26 @@ class Alarm:
         except ValueError:
             self.id = -1
 
+    def __set_date_time(self, date_line, time_line):
+        if len(time_line) == 4:
+            time_line = time_line + '00'
+        datetime_object = dt.strptime(f'{date_line} {time_line}', "%y%m%d %H%M%S")
+        if self.is_active:
+            self.raising_time = datetime_object
+        else:
+            self.ceasing_time = datetime_object
+
     def __parse_header(self, header_parts) -> bool:
-        type_identity = re.findall(r'[A|O][1-3]\/?\w{3}', header_parts[0])
+        type_identity = re.findall(ALARM_TYPE_REGEXP, header_parts[0])
         if not len(type_identity):
             return False
+
         self.type = type_identity[0]
         info_line = [x for x in header_parts[0].split(' ') if x != '']
         self.__parse_id(header_parts[0], info_line)
-
         time_line, date_line = info_line[-1], info_line[-2]
-        if len(time_line) == 4:
-            time_line = time_line + '00'
-        if self.is_active:
-            self.raising_time = dt.strptime(f'{date_line} {time_line}', "%y%m%d %H%M%S")
-        else:
-            self.ceasing_time = dt.strptime(f'{date_line} {time_line}', "%y%m%d %H%M%S")
+        self.__set_date_time(date_line, time_line)
+
         self.descr = header_parts[-1]
         return len(self.type) > 0
 
@@ -63,56 +74,32 @@ class Alarm:
 
         return result
 
+    @staticmethod
+    def __get_value_from_keys(container: dict, keys: list):
+        for key in keys:
+            if key in container.keys():
+                return container[key]
+        return ''
+
     def __set_values(self, content_info) -> None:
-        keys = content_info.keys()
-
-        if 'RSITE' in keys:
-            self.object_name = content_info['RSITE']
-        elif 'TG' in keys:
-            self.object_name = content_info['TG']
-        elif 'SAID' in keys:
-            self.object_name = content_info['SAID']
-        elif 'SPID' in keys:
-            self.object_name = content_info['SPID']
-
-        if 'MO' in keys:
-            self.managed_object = content_info['MO']
-        elif 'CELL' in keys:
-            self.managed_object = content_info['CELL']
+        self.object_name = self.__get_value_from_keys(content_info, object_name_keys)
+        self.managed_object = self.__get_value_from_keys(content_info, managed_object_keys)
+        self.slogan = self.__get_value_from_keys(content_info, slogan_keys)
+        if 'CELL' in content_info.keys():
             self.object_name = content_info['CELL'][:-1]
-        elif 'DIP' in keys:
-            self.managed_object = content_info['DIP']
-        elif 'SDIP' in keys:
-            self.object_name = content_info['SDIP']
-            self.managed_object = content_info['LAYER']
-        elif 'DEST' in keys:
-            self.managed_object = content_info['DEST']
-        elif 'UNIT' in keys:
-            self.managed_object = content_info['UNIT']
-        elif 'FILENAME' in keys:
-            self.managed_object = content_info['FILENAME']
-        elif 'SNT' in keys:
-            self.managed_object = content_info['SNT']
 
-        if 'ALARM_SLOGAN' in keys:
-            self.slogan = content_info['ALARM_SLOGAN']
-        elif 'FAULT' in keys:
-            self.slogan = content_info['FAULT']
-        elif 'FAULT_TYPE' in keys:
-            self.slogan = content_info['FAULT_TYPE']
-        elif 'REASON' in keys:
-            self.slogan = content_info['REASON']
-        elif 'STATE' in keys:
-            self.slogan = content_info['STATE']
+    @staticmethod
+    def __is_service_line(line: str) -> bool:
+        if line == '' or line.startswith('WO') \
+                or line.startswith('EX') or line == '\x03<':
+            return True
+        return False
 
     def __parse_content(self, alarm_data) -> None:
         alarm_data = alarm_data.replace('RADIO X-CEIVER ADMINISTRATION', '')
         if 'CEASING' in alarm_data:
             self.is_active = False
-        lines_repr = [x for x in alarm_data.split('\n') if x != ''
-                      and not x.startswith('WO')
-                      and not x.startswith('EX')
-                      and x != '\x03<']
+        lines_repr = [x for x in alarm_data.split('\n') if not self.__is_service_line(x)]
         if not len(lines_repr) or not self.__parse_header(lines_repr[0:2]):
             return
 
@@ -121,10 +108,8 @@ class Alarm:
             if 'DIGITAL PATH QUALITY SUPERVISION' in alarm_data:
                 self.slogan = lines_repr[2]
                 lines_repr.remove(lines_repr[2])
-            if len(lines_repr) == 3:
-                content_info = self.__get_values(lines_repr[1], lines_repr[2])
-            else:
-                content_info = self.__get_values(lines_repr[2], lines_repr[3])
+            start_line = 1 if len(lines_repr) == 3 else 2
+            content_info = self.__get_values(lines_repr[start_line], lines_repr[start_line + 1])
             self.__set_values(content_info)
         self.is_valid = True
 
